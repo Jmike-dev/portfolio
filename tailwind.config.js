04fdd4f48f9a4342e1b7c4c9366b1f12a161690c
Michael Gichuhi <gmikekamau@gmail.com>
2023-06-02 14:48:34 +0300

    ⬆ updated tailwind dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lora: "'DM Mono', monospace",
+        spartan: "'League Spartan', sans-serif",
+      },
+      colors: {
+        primary: {
+          "teal-green": "#24596D",
+          purple: "#9C69CC",
+          "soft-brown": "#D5CABD",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
