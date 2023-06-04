5d2edb83ba2b70ff2d00e054324d2e432cbdc388
Michael Gichuhi <gmikekamau@gmail.com>
2023-06-04 13:25:40 +0300

    💬 edited about paragraph and 🎉 began table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header-section/header/header.component';
 import { AboutComponent } from './components/header-section/about/about.component';
+import { ProjectsComponent } from './components/body-section/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
