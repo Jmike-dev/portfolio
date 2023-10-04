7c39b9c300d128b17e97fa14034322dfd3aaf973
Michael Gichuhi <gmikekamau@gmail.com>
2023-10-04 15:24:36 +0300

    ✨ defined my routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
+// modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+// components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './pages/home/header/header.component';
@@ -8,12 +12,16 @@ import { AboutComponent } from './pages/home/about/about.component';
 import { ProjectsComponent } from './pages/projects/projects/projects.component';
 import { TechnologyTableComponent } from './pages/home/technology-table/technology-table.component';
 import { ContactsComponent } from './layout/footer/contacts/contacts.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ReactiveFormsModule } from '@angular/forms';
 import { PopFormComponent } from './pop-ups/pop-form/pop-form.component';
 import { HomePageComponent } from './pages/home/home-page/home-page.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path: 'home', component: HomePageComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'about', component: AboutComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,15 +32,16 @@ import { NavbarComponent } from './layout/navbar/navbar.component';
     ContactsComponent,
     PopFormComponent,
     HomePageComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
