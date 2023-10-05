67558090b9bb0859b1be9cb4184b3f880f93f195
Michael Gichuhi <gmikekamau@gmail.com>
2023-10-05 19:28:23 +0300

    🚧 navbar build for desk view

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-
+import { faBars, faLaptop, faPerson } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
+ faLaptop = faLaptop
+ faHome = faHome
+ faPerson = faPerson
+ faBars = faBars
 
 }
