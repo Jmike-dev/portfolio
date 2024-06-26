7a437d067518a51f165ddea3ac544a58f1faa6c6
Michael Gichuhi <gmikekamau@gmail.com>
2024-06-26 18:38:25 +0300

    🔧 updated to use standalone comoponents

--- portfolio/src/app/pages/about/about.component.ts
+++ portfolio/src/app/pages/about/about.component.ts
@@ -3,10 +3,13 @@ import {
     cardYanimation,
     fadeInAnimation,
 } from 'src/assets/shared-animations/animations';
+import { ContactsComponent } from '../../components/footer/contacts/contacts.component';
 
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
     animations: [fadeInAnimation, cardYanimation],
+    standalone: true,
+    imports: [ContactsComponent],
 })
 export class AboutComponent {}
--- portfolio/src/app/pages/home/technology-table/technology-table.component.ts
+++ portfolio/src/app/pages/home/technology-table/technology-table.component.ts
@@ -4,11 +4,15 @@ import {
     cardYanimation,
 } from 'src/assets/shared-animations/animations';
 import {TechStack, Technologies} from 'src/techObject';
+import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
     selector: 'app-technology-table',
     templateUrl: './technology-table.component.html',
     animations: [cardXanimation, cardYanimation],
+    standalone: true,
+    imports: [NgFor, RouterLink],
 })
 export class TechnologyTableComponent {
     techStacks: Technologies[] = TechStack;
