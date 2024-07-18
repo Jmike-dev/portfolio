e8d0017b95f884887672a812dd20d8ed0f171b22
Michael Gichuhi <gmikekamau@gmail.com>
2024-07-18 18:29:09 +0300

    🚚 updated routing

---
+++ src/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from "@angular/router";
+import { ContactsComponent } from "./app/components/footer/contacts/contacts.component";
+import { AboutComponent } from "./app/pages/about/about.component";
+import { HomePageComponent } from "./app/pages/home/home-page/home-page.component";
+import { ProjectsComponent } from "./app/pages/projects/projects/projects.component";
+
+ export const routes: Routes = [
+     {path: 'home', component: HomePageComponent},
+     {path: 'projects', component: ProjectsComponent},
+     {path: 'about', component: AboutComponent},
+     {path: 'contact', component: ContactsComponent},
+     {path: '', redirectTo: 'home', pathMatch: 'full'},
+];
